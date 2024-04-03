c58897675bf0dcf1a125033cd6d18b13d6c38d3f
barakaganai <barakaganai01@gmail.com>
2024-04-03 21:01:28 +0300

    create index.tsx

---
+++ types/index.ts
@@ -0,0 +1,14 @@
+import { Manifest } from "next/dist/lib/metadata/types/manifest-types";
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
+}
+
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
