d69b409d1deae0c44bd9516262e3455c562d3d25
barakaganai <barakaganai01@gmail.com>
2024-04-06 23:48:54 +0300

    create fetchCars function

---
+++ utils/index.ts
@@ -0,0 +1,32 @@
+//function to call the API
+export const fetchCars = async () => {
+  const headers = {
+    "X-RapidAPI-Key": "1bb8749cc8msh6bad7e77b235989p18e42fjsn162de572f141",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    {
+      headers: headers,
+    }
+  );
+
+  const result = await response.json();
+  return result;
+};
+
+//calculate car rent..
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
