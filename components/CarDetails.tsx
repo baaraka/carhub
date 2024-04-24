7d19ae9a45317316e3cd1cae3e92c2463de6bc7e
barakaganai <barakaganai01@gmail.com>
2024-04-24 11:56:39 +0300

    fetch images from the index function

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { CarProps } from "@/types";
+import { generateCarImageUrl } from "@/utils";
 import { Dialog, Transition } from "@headlessui/react";
 import Image from "next/image";
 import React, { Fragment } from "react";
@@ -38,7 +39,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xl transition-all flex flex-col gap-5">
+                <Dialog.Panel className="relative p-6 w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xl transition-all flex flex-col gap-5">
                   <button
                     type="button"
                     onClick={closeModal}
@@ -53,7 +54,64 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                     />
                   </button>
                   <div className="flex-1 flex flex-col gap-3">
-                    <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg"></div>
+                    <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
+                      <Image
+                        src={generateCarImageUrl(car)}
+                        alt="card-image"
+                        fill
+                        priority
+                        className="object-contain"
+                      />
+                    </div>
+                    <div className="flex gap-3">
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src={generateCarImageUrl(car, "29")}
+                          alt="card-image"
+                          fill
+                          priority
+                          className="object-contain"
+                        />
+                      </div>
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src={generateCarImageUrl(car, "33")}
+                          alt="card-image"
+                          fill
+                          priority
+                          className="object-contain"
+                        />
+                      </div>
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src={generateCarImageUrl(car, "13")}
+                          alt="card-image"
+                          fill
+                          priority
+                          className="object-contain"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex-1 flex flex-col gap-2">
+                    <h2 className="font-semibold text-xl capitalize">
+                      {car.make} {car.model}
+                    </h2>
+                    <div className="mt-3 flex flex-wrap gap-4">
+                      {Object.entries(car).map(([key, value]) => (
+                        <div
+                          key={key}
+                          className="flex justify-between gap-5 w-full text-right"
+                        >
+                          <h4 className="text-grey capitalize">
+                            {key.split("_").join(" ")}
+                          </h4>
+                          <p className="text-black-100 text-semibold">
+                            {value}
+                          </p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
